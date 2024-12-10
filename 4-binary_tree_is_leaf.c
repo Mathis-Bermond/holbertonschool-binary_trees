937b3a565f5f449270a3e309eba4d9fb27da6c8b
yassinejsa21 <9872@holbertonstudents.com>
2024-12-10 15:22:15 +0100

    erreur de compilation task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,1 +1,28 @@
 #include "binary_trees.h"
+/**
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+
+{
+	binary_tree_is_leaf *node;
+
+	node = malloc(sizeof(*node));
+
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+
+	if (node->left == NULL && node->right == NULL)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+	return (const binary_tree_t);
+}
