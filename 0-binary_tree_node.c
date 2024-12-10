532cfc8068824a48d5e1ade5c0d6590fee58a3ca
Mathis-MB <9985@holbertonstudents.com>
2024-12-10 11:01:29 +0100

    feat: implement binary_tree_print function to visualize a binary tree structure fix: correct memory allocation and error handling in binary_tree_node function chore: add necessary includes for binary_tree_print functionality fix: ensure newline at end of file in binary_tree_inorder.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,27 +1,28 @@
 #include "binary_trees.h"
 /**
- */
+* binary_tree_node - Creates a binary tree node
+* @parent: A pointer to the parent node
+* @value: The value to put in the new node
+*
+* Return: A pointer to the new node, or NULL on failure
+*/
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 {
-        binary_tree_t = *new_node
+		binary_tree_t *new_node;
 
-        new_node = malloc( sizeof(*new_node));
+		new_node = malloc(sizeof(*new_node));
 
-        if(new_node == NULL)
-        {
-                printf(%s "error allocation memory \n");
+		if (new_node == NULL)
+		{
+			return (NULL);
+		}
 
-        }
+		new_node->n = value;
+		new_node->parent = parent;
+		new_node->left = NULL;
+		new_node->right = NULL;
 
-        new_node->value = n;
-        new_node->parent = NULL;
-        new_node->left = NULL;
-        new_node->right = NULL;
-
-        printf("create of %d\n" ,new_node->value);
-
-        free(new_node)
-        return(new_node)
+		return (new_node);
 }
