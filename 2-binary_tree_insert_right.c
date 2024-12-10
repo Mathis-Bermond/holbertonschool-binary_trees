6ada8a9b0990908716b50338dc12b13425dcbd94
yassinejsa21 <9872@holbertonstudents.com>
2024-12-10 14:49:13 +0100

    defaut de compilation task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,1 +1,23 @@
 #include "binary_trees.h"
+/**
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(*new_node));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = parent->right;
+
+	return (new_node)
+}
+
