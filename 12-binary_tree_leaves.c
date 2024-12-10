b951f7b92f9cdb1daa7c9459594a1adc814bf0e2
yassinejsa21 <9872@holbertonstudents.com>
2024-12-11 00:40:04 +0100

    manque compilation et doute sur le return task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,1 +1,18 @@
 #include "binary_trees.h"
+/**
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+
+	return ((tree->left) && (tree->right));
+}
