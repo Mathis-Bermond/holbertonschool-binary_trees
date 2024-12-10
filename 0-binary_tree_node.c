50d04befb81e1c080659b7631a28aeec4842f587
yassinejsa21 <9872@holbertonstudents.com>
2024-12-10 10:47:23 +0100

    rectification de la task 0 pour compilation

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,1 +1,27 @@
 #include "binary_trees.h"
+/**
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+{
+        binary_tree_t = *new_node
+
+        new_node = malloc( sizeof(*new_node));
+
+        if(new_node == NULL)
+        {
+                printf(%s "error allocation memory \n");
+
+        }
+
+        new_node->value = n;
+        new_node->parent = NULL;
+        new_node->left = NULL;
+        new_node->right = NULL;
+
+        printf("create of %d\n" ,new_node->value);
+
+        free(new_node)
+        return(new_node)
+}
