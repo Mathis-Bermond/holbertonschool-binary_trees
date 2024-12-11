4bc03ed016fb9246faaf18d7415a4a5e907ba7bc
MBRorry <9985@holbertonstudents>
2024-12-11 09:55:13 +0100

    fix: correct post-order traversal function calls in binary_tree_postorder

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,15 +13,14 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	new_node = malloc(sizeof(*new_node));
-
-	if (parent == NULL || value == 0)
-		return (NULL);
-	if (new_node == NULL)
+	if (parent == NULL)
 		return (NULL);
 
 	new_node = binary_tree_node(parent, value);
 
+	if (new_node == NULL)
+		return (NULL);
+	new_node = binary_tree_node(parent, value);
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
