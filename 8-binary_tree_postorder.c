41e5b77d66289be293c9143708b6af3edf4bea8a
MBRorry <9985@holbertonstudents>
2024-12-11 10:35:27 +0100

    fix: implement post-order traversal in binary_tree_postorder function

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -5,7 +5,7 @@
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
  */
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 
 {
 	if (tree == NULL || func == NULL)
@@ -15,5 +15,4 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
 	func(tree->n);
-
 }
