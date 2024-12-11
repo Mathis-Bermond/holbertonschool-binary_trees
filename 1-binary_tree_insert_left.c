4699dadb1747c33bf65cba9a8ef59d264061ad14
MBRorry <9985@holbertonstudents>
2024-12-11 10:09:35 +0100

    fix: resolve duplicate node creation in binary_tree_insert_left function

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,21 +12,22 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
+	binary_tree_t *temp;
 
 	if (parent == NULL)
 		return (NULL);
 
 	new_node = binary_tree_node(parent, value);
-
 	if (new_node == NULL)
 		return (NULL);
 	new_node = binary_tree_node(parent, value);
 	if (parent->left != NULL)
 	{
+		temp = parent->left;
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
+		new_node->left = temp;
 	}
 	parent->left = new_node;
-
 	return (new_node);
 }
