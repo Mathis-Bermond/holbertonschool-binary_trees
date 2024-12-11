afe8bc7ae9183cbdf6e15e02d298a2a3afab9c95
MBRorry <9985@holbertonstudents>
2024-12-11 10:22:26 +0100

    fix: correct binary_tree_is_leaf function to properly check for leaf nodes

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,28 +1,20 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: pointer to the node to check
+ *
+ * Return: 1 if node is a leaf, otherwise 0
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
-
 {
-	binary_tree_is_leaf *node;
-
-	node = malloc(sizeof(*node));
-
 	if (node == NULL)
-	{
-		return (NULL);
-	}
-
-	if (node->left == NULL && node->right == NULL)
-	{
-		return (1);
-	}
-
-	else
 	{
 		return (0);
 	}
-
-	return (const binary_tree_t);
+	if (node->left == NULL && node->right == NULL)
+	{
+		return (0);
+	}
+	return (0);
 }
