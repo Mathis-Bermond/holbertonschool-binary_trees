d0089985054378ed98f51ec40746b9678b7e73e9
MBRorry <9985@holbertonstudents>
2024-12-11 15:12:18 +0100

    fix: implement binary_tree_is_perfect function to check if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,17 +1,20 @@
 #include "binary_trees.h"
 /**
- */
+* binary_tree_is_perfect - Checks if a binary tree is perfect.
+* @tree: A pointer to the root node of the tree to check.
+*
+* Return: 1 if the tree is perfect, 0 otherwise.
+*/
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-        if (tree == NULL)
-        {
-                return (0);
-        }
-
-        if (tree->left == NULL && tree->right == NULL)
-        {
-                return (1);
-        }
-
-        return(binary_tree_is_perfect(tree->left));
+	if (tree == NULL)
+		return (0);
+	if (binary_tree_is_full(tree) && binary_tree_balance(tree) == 0)
+	{
+		if (binary_tree_is_perfect(tree->left) &&
+		binary_tree_is_perfect(tree->right))
+			return (1);
+		return (0);
+	}
+	return (0);
 }
