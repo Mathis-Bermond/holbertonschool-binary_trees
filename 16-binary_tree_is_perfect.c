10067b695c15ba20232cd91b5ef611aeebdf596a
yassinejsa21 <9872@holbertonstudents.com>
2024-12-11 15:04:31 +0100

    manque une partie de code mais je narrive pas a trouver

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,1 +1,17 @@
 #include "binary_trees.h"
+/**
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+        if (tree == NULL)
+        {
+                return (0);
+        }
+
+        if (tree->left == NULL && tree->right == NULL)
+        {
+                return (1);
+        }
+
+        return(binary_tree_is_perfect(tree->left));
+}
